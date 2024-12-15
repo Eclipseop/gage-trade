b829ff5a6180298ddf120b12fe4ccb23f75abc42
eclipseop <mh@otterdev.com>
2024-12-15 13:18:01 -0600

    unkonwn > any

--- src/preload.ts
+++ src/preload.ts
@@ -1,10 +1,10 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("api", {
-  send: (channel: string, data: any) => {
+  send: (channel: string, data: unknown) => {
     ipcRenderer.send(channel, data);
   },
-  receive: (channel: string, func: (arg0: any) => void) => {
+  receive: (channel: string, func: (arg0: unknown) => void) => {
     ipcRenderer.on(channel, (event, ...args) => func(args));
   },
 });
