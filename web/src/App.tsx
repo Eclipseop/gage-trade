ac472e4813078ccd978fdffd3d0e76b2ce2b1614
eclipseop <mh@otterdev.com>
2024-12-15 20:52:22 -0600

    toast, needs adjustment

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { type TradeListing, lookup, openTradeQuery } from "./trade/trade";
 import { api } from "./util/electron";
+import { toast, Toaster } from "sonner";
 
 export type ItemData = {
   name: string;
@@ -58,7 +59,16 @@ const App = () => {
         ...mods,
         affixs: mods.affixs.filter((affix) => affix.checked),
       };
-      console.log("xD");
+      // console.log("xD");
+
+      toast.promise(lookup(filteredMods), {
+        loading: "Loading...",
+        success: (data) => {
+          return "good job!";
+        },
+        error: "Damn... lmao",
+      });
+
       const items = await lookup(filteredMods);
       if (!items) {
         console.log("wjat the sigma? lookup returned undefined!!");
@@ -82,68 +92,72 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col mx-auto pt-2 px-2 text-neutral-200 bg-black min-h-screen font-serif">
-      <span className="text-xl">
-        {mods?.name} - <span className="font-semibold">{mods?.itemClass}</span>
-      </span>
+    <>
+      <Toaster />
+      <div className="flex flex-col mx-auto pt-2 px-2 text-neutral-200 bg-black min-h-screen font-serif">
+        <span className="text-xl">
+          {mods?.name} -{" "}
+          <span className="font-semibold">{mods?.itemClass}</span>
+        </span>
 
-      <form className="flex flex-col space-y-2 text-sm">
-        <div className="pl-1 grid grid-cols-2">
-          {mods?.affixs.map((a, idx) => (
-            <div
-              key={a.affix[0].poe_id}
-              className="space-x-1 flex items-center"
-              onClick={() => toggleChecked(idx)}
-              onKeyDown={() => toggleChecked(idx)}
+        <form className="flex flex-col space-y-2 text-sm">
+          <div className="pl-1 grid grid-cols-2">
+            {mods?.affixs.map((a, idx) => (
+              <div
+                key={a.affix[0].poe_id}
+                className="space-x-1 flex items-center"
+                onClick={() => toggleChecked(idx)}
+                onKeyDown={() => toggleChecked(idx)}
+              >
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={a.checked}
+                    onChange={() => toggleChecked(idx)}
+                  />
+                  {a.affix[0].rawText}
+                </label>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex space-x-1">
+            <button
+              type="button"
+              className="px-3 py-1 bg-black border border-white hover:bg-white hover:text-black rounded text-white w-full"
+              onClick={submitSearch}
             >
-              <label>
-                <input
-                  type="checkbox"
-                  checked={a.checked}
-                  onChange={() => toggleChecked(idx)}
-                />
-                {a.affix[0].rawText}
-              </label>
+              Submit
+            </button>
+            <button
+              type="button"
+              className="px-3 py-1 bg-black border border-white hover:bg-white hover:text-black rounded text-white"
+              onClick={submitTradeOpen}
+            >
+              Trade
+            </button>
+          </div>
+        </form>
+        <span className="flex flex-col text-sm pt-1">
+          {itemRes.map((ir, idx) => (
+            <div
+              className="flex flex-row justify-between leading-tight"
+              key={`res-${
+                // biome-ignore lint/suspicious/noArrayIndexKey: replace this once i am returing better info
+                idx
+              }`}
+            >
+              <span>
+                {ir.listing.price.amount} {ir.listing.price.currency}
+              </span>
+              <span className="text-xs text-neutral-500">
+                {ir.listing.account.name}
+              </span>
             </div>
           ))}
-        </div>
-
-        <div className="flex space-x-1">
-          <button
-            type="button"
-            className="px-3 py-1 bg-black border border-white hover:bg-white hover:text-black rounded text-white w-full"
-            onClick={submitSearch}
-          >
-            Submit
-          </button>
-          <button
-            type="button"
-            className="px-3 py-1 bg-black border border-white hover:bg-white hover:text-black rounded text-white"
-            onClick={submitTradeOpen}
-          >
-            Trade
-          </button>
-        </div>
-      </form>
-      <span className="flex flex-col text-sm pt-1">
-        {itemRes.map((ir, idx) => (
-          <div
-            className="flex flex-row justify-between leading-tight"
-            key={`res-${
-              // biome-ignore lint/suspicious/noArrayIndexKey: replace this once i am returing better info
-              idx
-            }`}
-          >
-            <span>
-              {ir.listing.price.amount} {ir.listing.price.currency}
-            </span>
-            <span className="text-xs text-neutral-500">
-              {ir.listing.account.name}
-            </span>
-          </div>
-        ))}
-      </span>
-    </div>
+        </span>
+      </div>
+    </>
   );
 };
 
