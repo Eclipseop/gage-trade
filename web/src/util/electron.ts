7274c42594346665a2605c47326b8cba14fc133b
eclipseop <mh@otterdev.com>
2024-12-19 13:16:19 -0600

    add types to the electrion ipc stuff

--- web/src/util/electron.ts
+++ web/src/util/electron.ts
@@ -1,3 +1,13 @@
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-const w: any = window;
+type IpcComms = {
+  send: (channel: string, data: unknown) => void;
+  receive: (channel: string, func: (data: string) => void) => void;
+};
+
+declare global {
+  interface Window {
+    api: IpcComms;
+  }
+}
+
+const w = window;
 export const api = w.api;
