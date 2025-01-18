0b21ec64f69c171275e9dd5829f5c7291b915997
eclipseop <mh@otterdev.com>
2025-01-18 17:11:41 -0600

    Move affix to it's own comp

---
+++ web/src/components/affix.tsx
@@ -0,0 +1,63 @@
+import type { AffixInfo } from "@/types/parser";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import { Input } from "./ui/input";
+
+type AffixProps = {
+  affix: AffixInfo;
+  roll: number | undefined;
+  min: number | undefined;
+  max: number | undefined;
+  checked: boolean;
+  onRollChange: (newRoll: number, type: "min" | "max") => void;
+  onCheckedChange: (checked: boolean) => void;
+};
+
+const Affix = ({
+  affix,
+  roll,
+  min,
+  max,
+  checked,
+  onRollChange,
+  onCheckedChange,
+}: AffixProps) => {
+  return (
+    <div className="flex items-center justify-between py-1">
+      <div className="flex items-center space-x-2 flex-grow">
+        <Checkbox
+          checked={checked}
+          onCheckedChange={onCheckedChange}
+          id={`checkbox-${affix.poe_id}`}
+        />
+        <label
+          htmlFor={`checkbox-${affix.poe_id}`}
+          className={`text-sm ${
+            checked ? "text-foreground" : "text-muted-foreground"
+          }`}
+        >
+          {affix.rawText}
+        </label>
+      </div>
+      {roll !== undefined && (
+        <div className="flex">
+          <Input
+            type="number"
+            value={min}
+            onChange={(e) => onRollChange(Number(e.target.value), "min")}
+            className="w-16 h-6 text-xs rounded-r-none"
+            disabled={!checked}
+          />
+          <Input
+            type="number"
+            value={max}
+            onChange={(e) => onRollChange(Number(e.target.value), "max")}
+            className="w-16 h-6 text-xs rounded-l-none"
+            disabled={!checked}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Affix;
--- web/src/components/poe-item-search.tsx
+++ web/src/components/poe-item-search.tsx
@@ -1,74 +1,15 @@
 import { sanitize } from "@/parser/item-parser";
 import type { TradeListing } from "@/trade/trade";
-import type { AffixInfo, ItemStat, SearchableItemData } from "@/types/parser";
+import type { ItemStat, SearchableItemData } from "@/types/parser";
 import dayjs from "dayjs";
 import { Globe, Search } from "lucide-react";
+import Affix from "./affix";
 import ToggleBadge from "./toggle-badge";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
-import { Checkbox } from "./ui/checkbox";
-import { Input } from "./ui/input";
 import { ScrollArea } from "./ui/scroll-area";
 import { Separator } from "./ui/separator";
-
-type AffixProps = {
-  affix: AffixInfo;
-  roll: number | undefined;
-  min: number | undefined;
-  max: number | undefined;
-  checked: boolean;
-  onRollChange: (newRoll: number, type: "min" | "max") => void;
-  onCheckedChange: (checked: boolean) => void;
-};
-
-const Affix = ({
-  affix,
-  roll,
-  min,
-  max,
-  checked,
-  onRollChange,
-  onCheckedChange,
-}: AffixProps) => {
-  return (
-    <div className="flex items-center justify-between py-1">
-      <div className="flex items-center space-x-2 flex-grow">
-        <Checkbox
-          checked={checked}
-          onCheckedChange={onCheckedChange}
-          id={`checkbox-${affix.poe_id}`}
-        />
-        <label
-          htmlFor={`checkbox-${affix.poe_id}`}
-          className={`text-sm ${
-            checked ? "text-foreground" : "text-muted-foreground"
-          }`}
-        >
-          {affix.rawText}
-        </label>
-      </div>
-      {roll !== undefined && (
-        <div className="flex">
-          <Input
-            type="number"
-            value={min}
-            onChange={(e) => onRollChange(Number(e.target.value), "min")}
-            className="w-16 h-6 text-xs rounded-r-none"
-            disabled={!checked}
-          />
-          <Input
-            type="number"
-            value={max}
-            onChange={(e) => onRollChange(Number(e.target.value), "max")}
-            className="w-16 h-6 text-xs rounded-l-none"
-            disabled={!checked}
-          />
-        </div>
-      )}
-    </div>
-  );
-};
 
 function getTimeDifference(dateString: string) {
   const now = dayjs();
